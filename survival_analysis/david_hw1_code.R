c1953c28035b96807987e9d5440e97734c07012b
dandexler <dandexler@gmail.com>
2019-11-06 21:31:32 -0500

    Added survival function and plots

--- survival_analysis/david_hw1_code.R
+++ survival_analysis/david_hw1_code.R
@@ -59,4 +59,63 @@ kruskal_wallis_model <- kruskal.test(new_hurricane$hour~new_hurricane$reason)
 # Recommendation: Recommended to use median, recommendation to utilize survival curve differences. 
 
 
+########################################
+# Survival Analysis
+########################################
+
+# Create survival function. Tenure is hour and event is survive = 0. Event indicates objects NOT censored, i.e. pumps that failed.
+hurricane_survival <- Surv(time = hurricane$hour, event = hurricane$survive == 0)
+
+# Kaplan-Meier Method to estimate survival function
+hurricane_km <- survfit(hurricane_surv ~ 1, data = hurricane)
+summary(hurricane_km)
+
+# Plot survival function
+p <- ggsurvplot(hurricane_km,
+           color = "steelblue3", 
+           risk.table = TRUE,
+           xlab = "Failure Time (Hours)",
+           ylab = "Pump Survival Probability",
+           legend="right",
+           theme = theme_bw(),
+           title = "Survival Function for all Pump Stations During Hurricane Katrina"
+           )
+
+#p$plot <- p$plot +
+ # scale_x_continuous(breaks = sort(c(seq(0, 48, 8))))
+
+#p$table <- p$table + scale_x_continuous(breaks = sort(c(seq(0, 48, 6))))
+
+# Subset hurricane by just failure types
+hurricane_only_failures = hurricane[which(hurricane$reason!=0),]
+
+# New survival object for only failures
+surv_hurricane_fails <- Surv(time = hurricane_only_failures$hour, event = hurricane_only_failures$survive == 0)
+
+# Stratified analysis to see if there is a difference in the survival functions using Wilcoxon
+survdiff(surv_hurricane_fails ~ reason, rho = 1, data = hurricane_only_failures)
+# Result: Chisq = 205, df = 3, p<2e-16
+
+# Fits all curves and plots
+hurricane_strat = survfit(Surv(time = hour, event = survive == 0) ~ reason, data = hurricane_only_failures)
+
+#  Plot the stratafied survival analysis
+ggsurvplot(hurricane_strat, data = hurricane_sub,
+           legend.title = "Failure Type",
+           legend.labs = c("flood", "motor", "surge", "jammed"),
+           xlab = "Failure Time (hours)", 
+           ylab = "Pump Survival Probability",
+           title="Stratified Survival Analysis of Pumps by Failure Type",
+           break.y.by = 0.1,
+           legend = "right",
+           conf.int = TRUE,
+           risk.table = TRUE,
+           tables.height = 0.2,
+           tables.theme = theme_cleantable(),
+           palette = "Dark",
+           ggtheme = theme_bw()
+)
+
+
+
 
